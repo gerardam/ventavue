3374591c26e9215e3d3adc2fce73306b8ead6a32
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2022-06-01 13:33:54 -0500

    Maqutedo de interfaz y link categoria en funciona.

--- src/App.vue
+++ src/App.vue
@@ -1,202 +1,169 @@
 <template>
-  <v-app id="inspire">
+  <v-app id="app">
     <v-navigation-drawer
-      v-model="drawer"
-      :clipped="$vuetify.breakpoint.lgAndUp"
       fixed
+      :clipped="$vuetify.breakpoint.mdAndUp"
       app
+      v-model="drawer"
+      style="top:64px;z-index:1;"
     >
-      <v-list dense>
-        <template>
-          
-        </template>
+      <v-list>
+        <v-list-item :to="{ name: 'home'}">
+          <v-list-item-icon>
+            <v-icon>home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Inicio</v-list-item-title>
+        </v-list-item>
+
+        <v-list-group>
+          <template v-slot:activator>
+            <v-list-item-title>Almacén</v-list-item-title>
+          </template>
+            <v-list-item :to="{ name: 'categoria'}">
+              <v-list-item-icon>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Categorías</v-list-item-title>
+            </v-list-item>
+            <v-list-item :to="{ name: ''}">
+              <v-list-item-icon>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Artículos</v-list-item-title>
+            </v-list-item>
+        </v-list-group>
+
+        <v-list-group>
+          <template v-slot:activator>
+            <v-list-item-title>Compras</v-list-item-title>
+          </template>
+            <v-list-item :to="{ name: ''}">
+              <v-list-item-icon>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Ingresos</v-list-item-title>
+            </v-list-item>
+            <v-list-item :to="{ name: ''}">
+              <v-list-item-icon>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Proveedores</v-list-item-title>
+            </v-list-item>
+        </v-list-group>
+
+        <v-list-group>
+          <template v-slot:activator>
+            <v-list-item-title>Ventas</v-list-item-title>
+          </template>
+            <v-list-item :to="{ name: ''}">
+              <v-list-item-icon>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Ventas</v-list-item-title>
+            </v-list-item>
+            <v-list-item :to="{ name: ''}">
+              <v-list-item-icon>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Clientes</v-list-item-title>
+            </v-list-item>
+        </v-list-group>
+
+        <v-list-group>
+          <template v-slot:activator>
+            <v-list-item-title>Accesos</v-list-item-title>
+          </template>
+            <v-list-item :to="{ name: ''}">
+              <v-list-item-icon>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Roles</v-list-item-title>
+            </v-list-item>
+            <v-list-item :to="{ name: ''}">
+              <v-list-item-icon>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Usuarios</v-list-item-title>
+            </v-list-item>
+        </v-list-group>
+
+        <v-list-group>
+          <template v-slot:activator>
+            <v-list-item-title>Consultas</v-list-item-title>
+          </template>
+            <v-list-item :to="{ name: ''}">
+              <v-list-item-icon>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Consultas compras</v-list-item-title>
+            </v-list-item>
+            <v-list-item :to="{ name: ''}">
+              <v-list-item-icon>
+                <v-icon>table_chart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Consultas ventas</v-list-item-title>
+            </v-list-item>
+        </v-list-group>
+        
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
       color="blue darken-3"
       dark
       app
-      fixed
+      :clipped-left="$vuetify.breakpoint.mdAndUp"
+      fixe
+      style="flex:none;z-index:2;"
     >
       <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <span class="hidden-sm-and-down">Google Contacts</span>
+        <span class="hidden-sm-and-down">Sistema</span>
       </v-toolbar-title>
-      <v-text-field
-        flat
-        solo-inverted
-        hide-details
-        prepend-inner-icon="search"
-        label="Search"
-        class="hidden-sm-and-down"
-      ></v-text-field>
       <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>apps</v-icon>
       </v-btn>
-      <v-btn icon>
-        <v-icon>notifications</v-icon>
-      </v-btn>
-      <v-btn icon large>
-        <v-avatar size="32px" tile>
-          <img
-            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
-            alt="Vuetify"
-          >
-        </v-avatar>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <v-container fluid fill-height>
-        <v-layout justify-center align-center>
-          <v-tooltip right>
-            <template v-slot:activator="{ on }">
-              <v-btn :href="source" icon large target="_blank" v-on="on">
-                <v-icon large>code</v-icon>
-              </v-btn>
-            </template>
-            <span>Source</span>
-          </v-tooltip>
-          <v-tooltip right>
-            <template v-slot:activator="{ on }">
-              <v-btn icon large href="https://codepen.io/johnjleider/pen/EQOYVV" target="_blank" v-on="on">
-                <v-icon large>mdi-codepen</v-icon>
-              </v-btn>
-            </template>
-            <span>Codepen</span>
-          </v-tooltip>
-        </v-layout>
+        <v-slide-y-transition mode="out-in">
+          <router-view/>
+        </v-slide-y-transition>
       </v-container>
     </v-content>
-    <v-btn
-      fab
-      bottom
-      right
-      color="pink"
-      dark
-      fixed
-      @click="dialog = !dialog"
-    >
-      <v-icon>add</v-icon>
-    </v-btn>
-    <v-dialog v-model="dialog" width="800px">
-      <v-card>
-        <v-card-title
-          class="grey lighten-4 py-4 title"
-        >
-          Create contact
-        </v-card-title>
-        <v-container grid-list-sm class="pa-4">
-          <v-layout row wrap>
-            <v-flex xs12 align-center justify-space-between>
-              <v-layout align-center>
-                <v-avatar size="40px" class="mr-3">
-                  <img
-                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                    alt=""
-                  >
-                </v-avatar>
-                <v-text-field
-                  placeholder="Name"
-                ></v-text-field>
-              </v-layout>
-            </v-flex>
-            <v-flex xs6>
-              <v-text-field
-                prepend-icon="business"
-                placeholder="Company"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs6>
-              <v-text-field
-                placeholder="Job title"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field
-                prepend-icon="mail"
-                placeholder="Email"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field
-                type="tel"
-                prepend-icon="phone"
-                placeholder="(000) 000 - 0000"
-                mask="phone"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field
-                prepend-icon="notes"
-                placeholder="Notes"
-              ></v-text-field>
-            </v-flex>
-          </v-layout>
-        </v-container>
-        <v-card-actions>
-          <v-btn flat color="primary">More</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn flat color="primary" @click="dialog = false">Cancel</v-btn>
-          <v-btn flat @click="dialog = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <v-footer height="auto" style="padding:0;z-index:0;text-align:center;">
+      <v-layout justify-center>
+        <v-flex text-xs-center>
+          <v-card flat tile color="primary" class="white--text">
+            <v-card-text class="white--text pt-0">
+              IncanatoIT &copy;2018
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+
   </v-app>
 </template>
-
-<script>
-  export default {
-    data: () => ({
-      dialog: false,
-      drawer: null,
-      items: [
-        { icon: 'contacts', text: 'Contacts' },
-        { icon: 'history', text: 'Frequently contacted' },
-        { icon: 'content_copy', text: 'Duplicates' },
-        {
-          icon: 'keyboard_arrow_up',
-          'icon-alt': 'keyboard_arrow_down',
-          text: 'Labels',
-          model: true,
-          children: [
-            { icon: 'add', text: 'Create label' }
-          ]
-        },
-        {
-          icon: 'keyboard_arrow_up',
-          'icon-alt': 'keyboard_arrow_down',
-          text: 'More',
-          model: false,
-          children: [
-            { text: 'Import' },
-            { text: 'Export' },
-            { text: 'Print' },
-            { text: 'Undo changes' },
-            { text: 'Other contacts' }
-          ]
-        },
-        { icon: 'settings', text: 'Settings' },
-        { icon: 'chat_bubble', text: 'Send feedback' },
-        { icon: 'help', text: 'Help' },
-        { icon: 'phonelink', text: 'App downloads' },
-        { icon: 'keyboard', text: 'Go to the old version' }
-      ]
-    }),
-    props: {
-      source: String
-    }
-  }
-</script>
 
 <script>
 
 export default {
   name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
 </script>
